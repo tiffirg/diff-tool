b26e25fcb73820d2771391db5903bdd7d942f454
Vadim Sannikov <vadim.sannikov.a@gmail.com>
2021-09-04 10:52:29 +0300

    Update project plan, added testing fun - `testOfFiles`

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -1,9 +1,41 @@
-import kotlin.test.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.PrintStream
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
 
 internal class Test1 {
 
+    private val standardOut = System.out
+    private val stream = ByteArrayOutputStream()
+
+    @BeforeTest
+    fun setUp() {
+        System.setOut(PrintStream(stream))
+    }
+
+    @AfterTest
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
     @Test
-    fun test1() {
-        assert(true)
+    fun testOfFiles() {
+        val primaryPathDirectory = "data/test"
+        for (i in 1..10) {
+            val pathDirectory = "$primaryPathDirectory$i/"
+            val pathFileOrigin ="${pathDirectory}origin.txt"
+            val pathFileNew = "${pathDirectory}new.txt"
+            val fileDiff = File("${pathDirectory}diff.txt")
+            val textFileDiff = fileDiff.readText()
+
+            val args = arrayOf<String>(pathFileOrigin, pathFileNew)
+
+            main(args)
+            assertEquals(textFileDiff, stream.toString().trim())
+        }
     }
 }
