bcc504c425b2d87774e41f00f4a229c31088f205
Vadim Sannikov <vadim.sannikov.a@gmail.com>
2021-09-14 21:14:08 +0300

    Added description about LCS algorithm

--- src/main/kotlin/services/algorithmLCS.kt
+++ src/main/kotlin/services/algorithmLCS.kt
@@ -6,12 +6,12 @@ import kotlin.math.max
 /**
  * Динамическая матрица для алгоритма LCS
  *
- * Объявление матрицы k + 1 на m + 1, где k и m - длины аргументов
+ * Объявление матрицы n + 1 на m + 1, где k и m - длины аргументов
  * Первые столбец и строка используются в качестве инициализации
- * Соотвественно 1..k и 1..m индексы предназначены для самой матрицы
+ * Соотвественно 1..n и 1..m индексы предназначены для самой матрицы
  *
- * @see <a href="https://hsto.org/storage2/4c7/061/02a/4c706102aa8f467337723aa092f4bd5a.gif">Наглядная реализация</a>
- *
+ * Проводя поиск решения для (n, m) находим решения для
+ * всех подзадач (k1, k2), меньших заданной.
  */
 private fun getDynamicMatrix(strings1: List<String>, strings2: List<String>): Array<Array<Int>> {
     val matrix = Array(strings1.size + 1) { Array(strings2.size + 1) { 0 } }
@@ -29,7 +29,11 @@ private fun getDynamicMatrix(strings1: List<String>, strings2: List<String>): Ar
 
 /**
  * Реализация алгоритма LCS
- *
+ * Начиная с последнего элемента, поднимаемся к началу по направлениям,
+ * заданным матрицей, и записываем символы в каждой позиции.
+ * Полученная последовательность начинается с конца, тогда
+ * перевернутая последовательность является результатом алгоритма.
+ * @see <a href="https://hsto.org/storage2/4c7/061/02a/4c706102aa8f467337723aa092f4bd5a.gif">Наглядная реализация</a>
  */
 fun applyLCS(strings1: List<String>, strings2: List<String>): List<String> {
     val matrix = getDynamicMatrix(strings1, strings2)
