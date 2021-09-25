2c6f30b810f3f495622c0734a5108273b0fe4db0
Vadim Sannikov <vadim.sannikov.a@gmail.com>
2021-09-25 16:08:08 +0300

    Refactoring code

--- src/main/kotlin/services/ArgsParser.kt
+++ src/main/kotlin/services/ArgsParser.kt
@@ -6,9 +6,8 @@ private const val AMOUNT_ARGS = 2
 
 class ArgsParser {
     fun parse(args: Array<String>): Arguments? {
-        if (args.size != AMOUNT_ARGS) {
+        if (args.size != AMOUNT_ARGS)
             return null
-        }
         return Arguments(args[0], args[1])
     }
 }
